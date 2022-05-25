6ce310f7edd3845fb50597e62df5422af7c824ba
ohdtaste6120 <102286556+ohdtaste6120@users.noreply.github.com>
2022-05-25 14:46:20 +0900

    Add files via upload

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,28 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
+
+    public GameObject losePanel;
+
+    public Text healthDisplay;
+
     public float speed;
     private float input;
 
     Rigidbody2D rb;
     Animator sinae;
+    AudioSource source;
 
     public int health;
 
+    public float startDashTime;
+    private float dashTime;
+    public float extraSpeed;
+    private bool isDashing;
+
     // Start is called before the first frame update
     void Start()
-    {   sinae = GetComponent<Animator>();
-        rb = GetComponent<Rigidbody2D>();    
+    {   
+        source = GetComponent<AudioSource>();
+        sinae = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        healthDisplay.text = health.ToString();    
     }
 
     
     // Update is called once per frame
-    void FixedUpdate()
+    private void Update()
     {
-        float input = Input.GetAxisRaw("Horizontal");
 
         if (input != 0)
         {
@@ -40,14 +54,38 @@ public class Player : MonoBehaviour
             transform.eulerAngles = new Vector3(0, 180, 0);
         }
 
+        if (Input.GetKeyDown(KeyCode.Space) && isDashing == false) {
+            speed += extraSpeed;
+            isDashing = true;
+            dashTime = startDashTime;
+        }
+
+        if (dashTime <= 0 && isDashing == true) 
+        {
+            isDashing = false;
+            speed -= extraSpeed;
+        }
+        else {
+            dashTime -= Time.deltaTime;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        input = Input.GetAxisRaw("Horizontal");
+
         rb.velocity = new Vector2(input * speed, rb.velocity.y);
     }
 
     public void TakeDamage(int damageAmount) {
+        source.Play();
         health -= damageAmount;
+        healthDisplay.text = health.ToString(); 
 
         if(health <= 0) {
+            losePanel.SetActive(true);
             Destroy(gameObject);
         }
     }
 }
+
